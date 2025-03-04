037126bd601a2ee6ec703c9378d8f4b3ab958612
Hugo dos Santos <victorhugo0856@gmail.com>
2025-03-04 10:23:29 -0300

    feat: created Data mock

---
+++ src/Datas.js
@@ -0,0 +1,36 @@
+export const news =
+{
+    title: "Noticia 1",
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl."
+    image: "https://picsum.photos/200/300",
+    likes: 2,
+    comments: 10,
+},
+{
+    title: "Noticia 2,
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl."
+    image: "https://picsum.photos/200/300",
+    likes: 2,
+    comments: 10,
+},
+{
+    title: "Noticia 3",
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl."
+    image: "https://picsum.photos/200/300",
+    likes: 2,
+    comments: 10,
+},
+{
+    title: "Noticia 4",
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl."
+    image: "https://picsum.photos/200/300",
+    likes: 2,
+    comments: 10,
+},
+{
+    title: "Noticia 5",
+    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl."
+    image: "https://picsum.photos/200/300",
+    likes: 2,
+    comments: 10,
+},
