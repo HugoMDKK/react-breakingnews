e5d6485a72199c1a23be91abebe587b3b7b0f23b
Hugo dos Santos <victorhugo0856@gmail.com>
2025-03-10 10:36:09 -0300

    feat: changes in the nav, card, global...

--- src/Datas.js
+++ src/Datas.js
@@ -1,36 +1,98 @@
-export const news =
-{
-    title: "Noticia 1",
-    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl."
-    image: "https://picsum.photos/200/300",
-    likes: 2,
-    comments: 10,
-},
-{
-    title: "Noticia 2,
-    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl."
-    image: "https://picsum.photos/200/300",
-    likes: 2,
-    comments: 10,
-},
-{
-    title: "Noticia 3",
-    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl."
-    image: "https://picsum.photos/200/300",
-    likes: 2,
-    comments: 10,
-},
-{
-    title: "Noticia 4",
-    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl."
-    image: "https://picsum.photos/200/300",
-    likes: 2,
-    comments: 10,
-},
-{
-    title: "Noticia 5",
-    text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl."
-    image: "https://picsum.photos/200/300",
-    likes: 2,
-    comments: 10,
-},
+export const news = [
+    {
+      title: "Noticia 1",
+      text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl.",
+      image: "https://picsum.photos/202/300",
+      likes: 2,
+      comments: 10,
+    },
+    {
+      title: "Noticia 2",
+      text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl.",
+      image: "https://picsum.photos/200/301",
+      likes: 2,
+      comments: 10,
+    },
+
+    {
+        title: "Noticia 3",
+        text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl.",
+        image: "https://picsum.photos/200/305",
+        likes: 2,
+        comments: 10,
+      },
+
+      {
+        title: "Noticia 4",
+        text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl.",
+        image: "https://picsum.photos/200/302",
+        likes: 2,
+        comments: 10,
+      },
+
+      {
+        title: "Noticia 5",
+        text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl.",
+        image: "https://picsum.photos/200/307",
+        likes: 2,
+        comments: 10,
+      },
+
+      {
+        title: "Noticia 6",
+        text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl.",
+        image: "https://picsum.photos/200/307",
+        likes: 2,
+        comments: 10,
+      },
+
+      {
+        title: "Noticia 7",
+        text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl.",
+        image: "https://picsum.photos/200/307",
+        likes: 2,
+        comments: 10,
+      },
+
+      {
+        title: "Noticia 8",
+        text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl.",
+        image: "https://picsum.photos/200/307",
+        likes: 2,
+        comments: 10,
+      },
+
+      {
+        title: "Noticia 9",
+        text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl.",
+        image: "https://picsum.photos/200/307",
+        likes: 2,
+        comments: 10,
+      },
+
+      {
+        title: "Noticia 10",
+        text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl.",
+        image: "https://picsum.photos/200/307",
+        likes: 2,
+        comments: 10,
+      },
+
+      {
+        title: "Noticia 11",
+        text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl.",
+        image: "https://picsum.photos/200/307",
+        likes: 2,
+        comments: 10,
+      },
+
+      {
+        title: "Noticia 12",
+        text: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl eget nisl.",
+        image: "https://picsum.photos/200/307",
+        likes: 2,
+        comments: 10,
+      },
+    // E assim por diante para as outras notícias.
+  ];
+  
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,17 @@
+import { Card } from "../../components/Card/Card.jsx";
 import { Navbar } from "../../components/Navbar/Navbar";
+import { news } from "../../Datas.js";
+import { HomeBody } from "./HomeStyled.jsx";
 
 export default function Home() {
   return (
-    <div>
-        <Navbar />
-      <h1>Home</h1>
-    </div>
-  )
+    <>
+      <Navbar />
+      <HomeBody>
+      {news.map((item, index) => {
+        return <Card key={index} news={item} />;
+      })}
+      </HomeBody>
+    </>
+  );
 }
